f04d77414090881d0ed4febe4a2eab251ba9960e
Ricardo Garcia Reis <91116535+ricardogarciareis@users.noreply.github.com>
2024-02-05 01:51:53 +0000

    Criacao do web service SOAP

---
+++ JAXWSSOAPcertidaoNascimento/src/test/java/com/dzrrcreations/JAXWSSOAPcertidaoNascimento/JaxwssoaPcertidaoNascimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzrrcreations.JAXWSSOAPcertidaoNascimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxwssoaPcertidaoNascimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
